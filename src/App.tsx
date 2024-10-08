1733ff762b3d77a3b7a4a36ad05f738463645d87
MRyutaro <m2002r1028@icloud.com>
2024-10-08 14:08:44 +0900

    add: borderとかマップサイズとか

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { Map } from "./components/Map";
 
 function App() {
-    return <Map />;
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <div
+                style={{
+                    width: "50vw",
+                    height: "50vh",
+                    border: "1px solid black",
+                }}
+            >
+                <Map />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,31 @@
-// タイルの左上のx, y座標、タイルの中身を受け取って描画するコンポーネント
-function Tile({ x, y, children }: { x: number; y: number; children: React.ReactNode }) {
+import { useEffect, useState } from "react";
+
+const TILE_SIDE_LENGTH = 100; // タイルのサイズ
+const ROTATE_X = 66; // マップの平たさ具合(deg)
+const MAP_OFFSET_X = 20; // マップのX方向のオフセット
+const MAP_OFFSET_Y = -21.7; // マップのY方向のオフセット
+
+function Tile({ x, y, children }: { x: number; y: number; children?: React.ReactNode }) {
+    const [tileHalfWidth, setTileHalfWidth] = useState<number>(0);
+    const [tileHalfHeight, setTileHalfHeight] = useState<number>(0);
+
+    useEffect(() => {
+        setTileHalfWidth(TILE_SIDE_LENGTH / Math.sqrt(2) + 0.7);  // 0.7は誤差補正。これがないとタイルの間のボーダーの太さが太すぎる。
+        setTileHalfHeight(TILE_SIDE_LENGTH / (Math.sqrt(2) * Math.tan((ROTATE_X * Math.PI) / 180)));
+    }, [TILE_SIDE_LENGTH]);
+
     return (
         <div
             style={{
-                position: 'absolute',
-                left: x * 100,
-                top: y * 100,
-                width: 100,
-                height: 100,
-                border: '1px solid black',
+                position: "absolute",
+                width: TILE_SIDE_LENGTH,
+                height: TILE_SIDE_LENGTH,
+                left: `${MAP_OFFSET_X + x * tileHalfWidth - y * tileHalfWidth}px`,
+                top: `${
+                    MAP_OFFSET_Y + y * (tileHalfWidth * Math.cos((ROTATE_X * Math.PI) / 180)) + x * (tileHalfWidth * Math.cos((ROTATE_X * Math.PI) / 180))
+                }px`,
+                transform: `rotateX(${ROTATE_X}deg) rotateZ(45deg)`,
+                border: "1px solid black",
             }}
         >
             {children}
@@ -17,16 +34,57 @@ function Tile({ x, y, children }: { x: number; y: number; children: React.ReactN
 }
 
 export function Map() {
+    // ここで連想配列を取得
+    // 連想配列に画像のパスを直接書くか、他の管理方法にするかは考える
+    // 連想配列のキーは、タイルの座標を表す文字列にする
+    // 例: "0,0" -> (0, 0)のタイル
+    // キーがない部分はデフォルトのタイルで埋める
+    // tilesNum.x, tilesNum.y でアクセスできるようにする
+    const [tilesNum, setTilesNum] = useState({ x: 3, y: 3 });
+
+    useEffect(() => {
+        // ウィンドウサイズが変更されたときにタイルの数を再計算
+        const handleResize = () => {
+            setTilesNum({
+                x: Math.ceil(window.innerWidth / TILE_SIDE_LENGTH),
+                y: Math.ceil(window.innerHeight / TILE_SIDE_LENGTH),
+            });
+        };
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
         <div
             style={{
-                position: 'relative',
+                position: "relative",
+                width: "100%",
+                height: "100%",
             }}
         >
-            <Tile x={0} y={0}>A</Tile>
-            <Tile x={1} y={0}>B</Tile>
-            <Tile x={2} y={0}>C</Tile>
-            <Tile x={0} y={1}>D</Tile>
+            <div
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    padding: "8px",
+                    background: "lightgray",
+                }}
+            >
+                {tilesNum.x} x {tilesNum.y}
+            </div>
+            <Tile x={0} y={0}>
+                (0, 0)
+            </Tile>
+            <Tile x={1} y={-1}>
+                (1, -1)
+            </Tile>
+            <Tile x={2} y={-2}>
+                (2, -2)
+            </Tile>
         </div>
     );
 }
